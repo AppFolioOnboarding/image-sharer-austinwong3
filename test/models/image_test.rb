0b5004c38444e16c2554f86d595cb5074e91c05f
austinwong3 <austin.wong@appfolio.com>
2021-04-29 01:30:10 -0700

    Add Image Model Tests

--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class ImageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_image__valid
+    img = Image.new(url: 'https://picsum.photos/id/237/200/300')
+
+    assert_predicate img, :valid?
+  end
+
+  def test_image__invalid_if_blank
+    img = Image.new(url: '')
+
+    assert_not_predicate img, :valid?
+    assert_equal 'can\'t be blank', img.errors.messages[:url].first
+  end
+
+  def test_image__invalid_if_not_url
+    img = Image.new(url: 'asdf')
+
+    assert_not_predicate img, :valid?
+    assert_equal 'is invalid', img.errors.messages[:url].first
+  end
 end
