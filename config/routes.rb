81bfcf5926d2adb29e1774e939d467d9a311cf73
austinwong3 <austin.wong@appfolio.com>
2021-05-05 01:16:00 -0700

    Add Tag Filtered Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'images#index'
-
+  get 'tags/:tag', to: 'images#index', as: :tag
   resources :images, except: %i[edit update destroy]
 end
