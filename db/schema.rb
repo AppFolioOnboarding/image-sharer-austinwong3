d22b3fed11233ab5bf6c4c4355778b660a9d2426
austinwong3 <austin.wong@appfolio.com>
2021-05-06 15:28:32 -0700

    Remove Tags Column From Images

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_05_001914) do
+ActiveRecord::Schema.define(version: 2021_05_06_222749) do
 
   create_table "images", force: :cascade do |t|
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "tags"
   end
 
   create_table "taggings", force: :cascade do |t|
