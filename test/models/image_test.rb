a4f9db29a5636e0310e6c468ab963d958b5dcf08
austinwong3 <austin.wong@appfolio.com>
2021-05-06 08:59:13 -0700

    Update Image Model Test for Required Tags

--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -7,20 +7,21 @@ class ImageTest < ActiveSupport::TestCase
     assert_predicate img, :valid?
   end
 
-  def test_image__valid_if_no_tags
-    img = Image.new(url: 'https://picsum.photos/id/237/200/300', tag_list: nil)
+  def test_image__invalid_if_no_tags
+    img = Image.new(url: 'https://picsum.photos/id/237/200/300', tag_list: '')
 
-    assert_predicate img, :valid?
+    assert_not_predicate img, :valid?
+    assert_equal 'can\'t be blank', img.errors.messages[:tag_list].first
   end
 
-  def test_image__invalid_if_blank
+  def test_image__invalid_if_no_url
     img = Image.new(url: '', tag_list: 'cool, nice')
 
     assert_not_predicate img, :valid?
     assert_equal 'can\'t be blank', img.errors.messages[:url].first
   end
 
-  def test_image__invalid_if_not_url
+  def test_image__invalid_if_not_valid_url
     img = Image.new(url: 'asdf', tag_list: 'cool, nice')
 
     assert_not_predicate img, :valid?
