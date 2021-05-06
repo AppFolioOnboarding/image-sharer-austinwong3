cd61b4e6c51cba82cea98b181eb4eba6ee69e06d
austinwong3 <austin.wong@appfolio.com>
2021-05-06 02:33:30 -0700

    Require Tag for Model

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,5 @@
 class Image < ApplicationRecord
   validates :url, presence: true, format: URI::DEFAULT_PARSER.make_regexp(%w[http https])
+  validates :tag_list, presence: true
   acts_as_taggable_on :tags
 end
